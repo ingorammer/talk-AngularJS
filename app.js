bc66e196f3ee62864efe69d0482dc96f2a563a37
Ingo Rammer <ingo.rammer@thinktecture.com>
2013-09-01 07:34:30 +0200

    Added base infrastructure and simple model binding demo

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express')
+   , http = require('http')
+   , path = require('path');
+
+var app = express();
+
+app.configure(function () {
+   app.set('port', 3001);
+   app.use(express.favicon());
+   app.use(express.static(path.join(__dirname)));
+});
+
+http.createServer(app).listen(app.get('port'), function () {
+   console.log("Express server listening on port " + app.get('port'));
+});
---
+++ demos/modelbinding/demo.js
@@ -0,0 +1,10 @@
+var mod = angular.module("test", []);
+
+function DemoController($scope){
+
+   $scope.user = {
+      firstName: "John",
+      lastName: "Doe"
+   }
+
+}
